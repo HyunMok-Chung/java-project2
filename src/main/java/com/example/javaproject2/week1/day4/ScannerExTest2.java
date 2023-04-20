5b3c9dfc4e5deec41b50800f5bd9136193ac9475
HyunMok-Chung <1021hm@naver.com>
2023-04-20 10:48:26 +0900

    fix: 패키지 위치 수정

--- src/main/java/com/example/javaproject2/week1/day4/ScannerExTest2.java
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerExTest2.java
@@ -3,6 +3,6 @@ package com.example.javaproject2.week1.day4;
 public class ScannerExTest2 {
     public static void main(String[] args) {
         ScannerEx2 se = new ScannerEx2();
-        se.readTwoNumbersAndPlus2();
+        se.inputTwoNumbersAndPlus2();
     }
 }
