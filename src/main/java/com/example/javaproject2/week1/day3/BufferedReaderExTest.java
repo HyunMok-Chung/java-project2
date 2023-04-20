f3f83cbf446b7454d2b4159bb00df255268d9f9d
HyunMok-Chung <1021hm@naver.com>
2023-04-20 09:47:05 +0900

    fix: 패키지 정리

--- src/main/java/com/example/javaproject2/week1/day3/BufferedReaderExTest.java
+++ src/main/java/com/example/javaproject2/week1/day3/BufferedReaderExTest.java
@@ -1,6 +1,5 @@
-package com.example.javaproject2.week1;
+package com.example.javaproject2.week1.day3;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 
 public class BufferedReaderExTest {
