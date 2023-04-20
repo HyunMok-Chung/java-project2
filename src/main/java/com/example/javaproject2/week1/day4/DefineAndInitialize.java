9c2cd3a5f9cf92bdd658937ac275dad5672d40d4
HyunMok-Chung <1021hm@naver.com>
2023-04-20 15:27:43 +0900

    feat: ScannerAndVariable - 선언과 동시에 초기화하기

---
+++ src/main/java/com/example/javaproject2/week1/day4/DefineAndInitialize.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1.day4;
+
+import com.example.javaproject2.week1.day2.PrintHello;
+
+import java.util.Scanner;
+
+public class DefineAndInitialize {
+    public static void main(String[] args) {
+        int iVal = 1;  // 변수 선언과 초기화
+        PrintHello printHello = new PrintHello();
+    }
+}
