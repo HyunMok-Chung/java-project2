b7641f06a5259600d92a1d7b8d6a2103cfc3f9ec
HyunMok-Chung <1021hm@naver.com>
2023-04-20 15:08:07 +0900

    feat: ScannerFormatting

---
+++ src/main/java/com/example/javaproject2/week1/day4/ByteTypeExercise.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week1.day4;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = -127;
+//        byte byte3 = -129;
+//        byte byte4 = 128;
+        // 1byte눈 8bit이므로 -128~127까지 256개의 숫자만 저장 가능.
+
+        System.out.println(byte1);
+        System.out.println(byte2);
+//        System.out.println(byte3);
+//        System.out.println(byte4);
+    }
+}
