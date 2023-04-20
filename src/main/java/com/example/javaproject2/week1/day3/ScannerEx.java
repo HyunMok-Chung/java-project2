502143eebe973a0b8de8b84337a47007184cecda
HyunMok-Chung <1021hm@naver.com>
2023-04-20 10:38:29 +0900

    fix: 코드 수정

--- src/main/java/com/example/javaproject2/week1/day3/ScannerEx.java
+++ src/main/java/com/example/javaproject2/week1/day3/ScannerEx.java
@@ -3,10 +3,7 @@ package com.example.javaproject2.week1.day3;
 import java.util.Scanner;
 
 public class ScannerEx {
-    public void plusTwoNumbers(int val1, int val2){
-    }
-
-    public void readTwoNumbersAndPlus(){
+    public void inputTwoNumbersAndPlus(){
         // 숫자 2개 입력받기
         Scanner sc = new Scanner(System.in);
         System.out.println(sc.nextInt() + sc.nextInt());
