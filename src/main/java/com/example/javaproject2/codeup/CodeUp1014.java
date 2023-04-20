9a39be14633b26cd7086e5dbea44de52597c3844
HyunMok-Chung <1021hm@naver.com>
2023-04-20 15:33:40 +0900

    feat: CodeUp1014 문제풀이

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1014.java
@@ -0,0 +1,23 @@
+package com.example.javaproject2.codeup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.stream.StreamSupport;
+
+public class CodeUp1014 {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        char x = st.nextToken().charAt(0);
+//        char y = st.nextToken().charAt(0);
+//        System.out.printf("%c %c\n", y, x);
+
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        String y = sc.next();
+        System.out.printf("%s %s\n", y, x);
+    }
+}
