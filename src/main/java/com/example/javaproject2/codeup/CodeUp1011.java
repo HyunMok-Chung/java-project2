f7d2f98f612e5fea93fdc4a15566cee86301e52f
HyunMok-Chung <1021hm@naver.com>
2023-04-20 13:38:44 +0900

    feat: CodeUp 1011 ~ 1013 문제풀이

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1011.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.io.*;
+
+public class CodeUp1011 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c = br.readLine().charAt(0);
+        System.out.println(c);
+    }
+}
