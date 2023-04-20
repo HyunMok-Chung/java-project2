b79fa96b15af3f88b4cafb3a1ad58ee4a9cedf08
HyunMok-Chung <1021hm@naver.com>
2023-04-20 13:12:45 +0900

    fix: 구구단 2단 출력

--- src/main/java/com/example/javaproject2/week1/day4/MultiplicationTable.java
+++ src/main/java/com/example/javaproject2/week1/day4/MultiplicationTable.java
@@ -2,10 +2,16 @@ package com.example.javaproject2.week1.day4;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        // 출력할 문장
+        // 출력할 문장 (구구단 2단)
         // 2 * 1 = 2
-        // 2 * 2 = 4
-        System.out.println("2" + "*" + "1" + " = " + "2");
-        System.out.printf("%d * %d = %d", 2, 2, 2*2);
+        System.out.printf("%d * %d = %d\n", 2, 1, 2);
+        System.out.printf("%d * %d = %d\n", 2, 2, 2 * 2);
+        System.out.printf("%d * %d = %d\n", 2, 3, 2 * 3);
+        System.out.printf("%d * %d = %d\n", 2, 4, 2 * 4);
+        System.out.printf("%d * %d = %d\n", 2, 5, 2 * 5);
+        System.out.printf("%d * %d = %d\n", 2, 6, 2 * 6);
+        System.out.printf("%d * %d = %d\n", 2, 7, 2 * 7);
+        System.out.printf("%d * %d = %d\n", 2, 8, 2 * 8);
+        System.out.printf("%d * %d = %d\n", 2, 9, 2 * 9);
     }
 }
