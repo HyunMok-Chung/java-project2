b15fb9a3ea4acf50942c471d4da364cea710b2a4
HyunMok-Chung <1021hm@naver.com>
2023-04-24 15:40:37 +0900

    feat: CodeUp1028 ~ 1030 문제풀이

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1028.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1028 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long data = sc.nextLong();
+        System.out.println(data);
+    }
+}
