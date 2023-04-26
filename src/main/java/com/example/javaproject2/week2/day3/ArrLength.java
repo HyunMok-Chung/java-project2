e48133d6eca2ab6effea075d21925ba02ecec74b
HyunMok-Chung <1021hm@naver.com>
2023-04-26 14:12:55 +0900

    feat: 몫, 나머지 구하기

---
+++ src/main/java/com/example/javaproject2/week2/day3/ArrLength.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week2.day3;
+
+public class ArrLength {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        // 배열의 길이 메소드
+        System.out.println(arr.length);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week2/day3/RemainderQuote.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week2.day3;
+
+public class RemainderQuote {
+    public static void main(String[] args) {
+        int iVal = 1;
+        System.out.println(iVal / 2);
+
+        // 몫
+        int quote = 10 / 3;
+        // 나머지
+        int remainder = 10 % 3;
+        System.out.printf("몫: %d, 나머지: %d\n", quote, remainder);
+    }
+}
