5473833c061dd4dcf9e5cf76ca33d78e3fdf5de1
HyunMok-Chung <1021hm@naver.com>
2023-04-27 14:25:52 +0900

    feat: BitOperatorEx 비트 연산자

---
+++ src/main/java/com/example/javaproject2/week2/day4/BitOperatorEx.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week2.day4;
+
+public class BitOperatorEx {
+    public static void main(String[] args) {
+        int iVal1 = 1;
+        System.out.println(iVal1 << 1);  // 곱하기 계산
+        System.out.println(iVal1 << 2);
+        System.out.println(iVal1 << 3);
+
+        int iVal2 = 32;
+        System.out.println(iVal2 >> 1);  // 나누기 계산
+        System.out.println(iVal2 >> 2);
+    }
+}
