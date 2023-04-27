573a43f9370d3a2b0ab0c51a56c5974657fe989f
HyunMok-Chung <1021hm@naver.com>
2023-04-27 13:12:10 +0900

    feat: OR, NOT 논리 연산자 예제

---
+++ src/main/java/com/example/javaproject2/week2/day4/NotPoeratorEx.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week2.day4;
+
+public class NotPoeratorEx {
+    public static void main(String[] args) {
+        boolean result = !true;
+        System.out.printf("%b\n", result);
+
+        result = !false;
+        System.out.printf("%b\n", result);
+    }
+}
