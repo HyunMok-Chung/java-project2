cdb299d85f323117bae062ffdf655c159bf0a3a1
HyunMok-Chung <1021hm@naver.com>
2023-04-28 14:14:25 +0900

    feat: CodeUp 1116 ~ 1118

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1117.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1117 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+
+        System.out.printf("%.2f", num1 * num2);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1118.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1118 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.printf("%.1f", num1 * num2 / 2.0);
+    }
+}
