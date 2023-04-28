2666815e7cb0996e69d7039387d4c11a61128689
HyunMok-Chung <1021hm@naver.com>
2023-04-28 14:14:39 +0900

    feat: CodeUp 1119 ~ 1121

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1119.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1119 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+
+        System.out.print(num1 * 24);
+    }
+}
