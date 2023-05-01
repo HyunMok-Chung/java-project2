928bcefac53b21010b3b1e933e95a03cb67636f0
HyunMok-Chung <1021hm@naver.com>
2023-05-01 11:26:54 +0900

    feat: for문 실습

---
+++ src/main/java/com/example/javaproject2/week3/day1/ForLoopForwardHorizontal.java
@@ -0,0 +1,21 @@
+package com.example.javaproject2.week3.day1;
+
+public class ForLoopForwardHorizontal {
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            System.out.printf("%d ", i);
+        }
+
+
+        // 별 찍기.
+        // ****
+        // ****
+        // ****
+        /*
+        *
+        * *
+        * * *
+        * * * *
+         */
+    }
+}
