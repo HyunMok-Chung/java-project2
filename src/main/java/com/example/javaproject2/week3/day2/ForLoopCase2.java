2e04bda7c2cd2f23f3af32ce9ee067d0e3cd1bbf
HyunMok-Chung <1021hm@naver.com>
2023-05-02 11:24:02 +0900

    feat: ForLoopCase

---
+++ src/main/java/com/example/javaproject2/week3/day2/ForLoopCase2.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.week3.day2;
+
+public class ForLoopCase2 {
+    public static void main(String[] args) {
+        for(int i = 2 ; i == 1 ; i++){
+            System.out.println(i);
+        }
+
+        for (int i = 0; 2 * i <= 18 ; i++) {
+            System.out.println(i);
+        }
+
+        for(int i = 2; i * i <= 50 ; i++){
+            System.out.println(i);
+        }
+    }
+}
