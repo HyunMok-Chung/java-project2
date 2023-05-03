b293520868174ad1bd5f7d254d3e9299f5a86fb3
HyunMok-Chung <1021hm@naver.com>
2023-05-03 13:38:43 +0900

    feat: 소수 루트 n이하 검색

--- src/main/java/com/example/javaproject2/week3/day3/IsPrime2.java
+++ src/main/java/com/example/javaproject2/week3/day3/IsPrime2.java
@@ -4,12 +4,18 @@ public class IsPrime2 {
     public static void main(String[] args) {
         int num = 7;
         int factors = 0;
-        for (int i = 2; i <= num; i++) {
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
                 factors++;
             }
         }
         System.out.println("factors = " + factors);
         System.out.println(factors == 0);
+        if (factors == 0) {
+            System.out.println("prime");
+        }
+        else {
+            System.out.println("not prime");
+        }
     }
 }
