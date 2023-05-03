73fa41295f9b693b9180c6981b362c74b1734d45
HyunMok-Chung <1021hm@naver.com>
2023-05-03 10:07:31 +0900

    feat: CodeUp 1071, 1073  - do-while 풀이

--- src/main/java/com/example/javaproject2/codeup/CodeUp1071.java
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1071.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 public class CodeUp1071 {  // for문을 활용한 풀이
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for(int i = 0;;i++){
-            int iVal = sc.nextInt();
-            if(iVal == 0)
-                break;
-            System.out.println(iVal);
-        }
+//        for(int i = 0;;i++){
+//            int iVal = sc.nextInt();
+//            if(iVal == 0)
+//                break;
+//            System.out.println(iVal);
+//        }
+        // do-while 풀이
+        int iVal;
+        do {
+            iVal = sc.nextInt();
+            if(iVal != 0)
+                System.out.println(iVal);
+        } while (iVal == 0);
     }
 }
