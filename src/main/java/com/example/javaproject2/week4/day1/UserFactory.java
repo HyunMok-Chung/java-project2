b015a92b009adf4027755541d35409e9a565ff99
HyunMok-Chung <1021hm@naver.com>
2023-05-08 13:41:14 +0900

    feat: getAdultUser

---
+++ src/main/java/com/example/javaproject2/week4/day1/UserFactory.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week4.day1;
+
+public class UserFactory {
+    public User getAultUser() {
+        User user = new User();
+        user.age = 37;
+        return user;
+    }
+}
