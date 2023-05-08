f640b7a9506d733ee73cde9a0dc8a3d94725bde7
HyunMok-Chung <1021hm@naver.com>
2023-05-08 09:39:27 +0900

    feat: 파일명 수정

--- src/main/java/com/example/javaproject2/week4/day1/RightTriangleStar.java
+++ src/main/java/com/example/javaproject2/week4/day1/RightTriangleStar.java
@@ -2,7 +2,7 @@ package com.example.javaproject2.week4.day1;
 
 import java.util.Scanner;
 
-public class PrintStar {
+public class RightTriangleStar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int iVal = sc.nextInt();
