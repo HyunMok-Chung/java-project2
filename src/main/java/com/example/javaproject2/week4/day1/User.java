4fe78dc196e9b1d42619c58a41c182c267c417ea
HyunMok-Chung <1021hm@naver.com>
2023-05-11 10:29:41 +0900

    feat: Constructer, Getter, Setter 추가

--- src/main/java/com/example/javaproject2/week4/day1/User.java
+++ src/main/java/com/example/javaproject2/week4/day1/User.java
@@ -3,7 +3,46 @@ package com.example.javaproject2.week4.day1;
 public class User {
     String name;
     String phoneNumber;
+    String password;
     int age;
+
+    public User(String name, String phoneNumber, int age) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     boolean isAdult() {
         return age >= 18;
