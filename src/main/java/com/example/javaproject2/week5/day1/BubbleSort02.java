c9895f2cfa5259e96609b189a8fc0bc6b438189d
HyunMok-Chung <1021hm@naver.com>
2023-05-15 09:37:49 +0900

    fix: BubbleSort 튜닝 01

--- src/main/java/com/example/javaproject2/week5/day1/BubbleSort02.java
+++ src/main/java/com/example/javaproject2/week5/day1/BubbleSort02.java
@@ -6,8 +6,8 @@ public class BubbleSort02 {
     public static void main(String[] args) {
         int[] arr = {7, 2, 3, 9, 28, 11};
 
-        for (int j = 0; j < arr.length - 1; j++) {
-            for (int i = 0; i < arr.length - 1; i++) {
+        for (int j = 1; j <= arr.length; j++) {
+            for (int i = 0; i < arr.length - j; i++) {
                 if (arr[i] > arr[i + 1]) {
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
