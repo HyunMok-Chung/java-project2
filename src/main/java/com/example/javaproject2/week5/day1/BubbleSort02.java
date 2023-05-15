b91759ee59bd255129e6297d32b762fc6422e1b2
HyunMok-Chung <1021hm@naver.com>
2023-05-15 11:29:32 +0900

    feat: week5 day1  - BubbleSort2

--- src/main/java/com/example/javaproject2/week5/day1/BubbleSort02.java
+++ src/main/java/com/example/javaproject2/week5/day1/BubbleSort02.java
@@ -1,11 +1,18 @@
 package com.example.javaproject2.week5.day1;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class BubbleSort02 {
+    Comparator<Integer> comparator;
+
+    public BubbleSort02(Comparator<Integer> comparator) {
+        this.comparator = comparator;
+    }
+
     public int[] sortARound(int[] arr, int until) {
         for (int i = 0; i < until; i++) {
-            if (arr[i] > arr[i + 1]) {
+            if (comparator.compare(arr[i], arr[i + 1]) > 0) {
                 int temp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;
@@ -21,7 +28,13 @@ public class BubbleSort02 {
         return arr;
     }
     public static void main(String[] args) {
-        BubbleSort02 bubbleSort02 = new BubbleSort02();
+        BubbleSort02 bubbleSort02 = new BubbleSort02(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        }); // 기존 형태 DI
+//        BubbleSort02 bubbleSort02 = new BubbleSort02((o1, o2)-> o1-o2); // Lamda 형태 DI
         int[] arr = {7, 2, 3, 9, 28, 11};
 
         arr = bubbleSort02.sort(arr);
---
+++ src/main/java/com/example/javaproject2/week5/day1/BubbleSort2.java
@@ -0,0 +1,38 @@
+package com.example.javaproject2.week5.day1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BubbleSort2 {
+    Comparator<Integer> comparator;
+
+    public BubbleSort2(Comparator<Integer> comparator) {
+        this.comparator = comparator;
+    }
+
+    public int[] sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j <= arr.length - 1; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        BubbleSort2 bubbleSort2 = new BubbleSort2(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        int[] arr = {7, 2, 3, 9, 28, 1};
+
+        arr = bubbleSort2.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
