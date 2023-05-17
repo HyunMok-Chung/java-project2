006186b4654d85f449b3b2d680b559a6b5372b1a
HyunMok-Chung <1021hm@naver.com>
2023-05-17 10:48:31 +0900

    feat: week5 day3  - Stack push, pop, isEmpty, peek 구현

--- src/main/java/com/example/javaproject2/week5/day3/Stack1.java
+++ src/main/java/com/example/javaproject2/week5/day3/Stack1.java
@@ -1,6 +1,7 @@
 package com.example.javaproject2.week5.day3;
 
 import java.util.Arrays;
+import java.util.EmptyStackException;
 
 public class Stack1 {
     private int[] arr = new int[10000];
@@ -8,21 +9,33 @@ public class Stack1 {
 
     public void push(int value) {
         this.arr[pointer++] = value;
-        System.out.println(Arrays.toString(arr));
-        System.out.println("pointer = " + pointer);
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println("pointer = " + pointer);
     }
 
     public int pop() {
-        int temp = this.arr[pointer - 1];
-        pointer--;
-        return temp;
+//        int temp = this.arr[pointer - 1];
+//        pointer--;
+//        return temp;
 //        return this.arr[--pointer]; -> 한줄 코드
+        if(!isEmpty()){
+            return arr[--pointer];
+        } else {
+            throw new EmptyStackException();
+        }
     }
+    public boolean isEmpty() {
+        return this.pointer == 0;
+    }
+
+    public int peek() {
+        if (isEmpty())
+            throw new EmptyStackException();
+        return arr[pointer - 1];
+    }
+
     public static void main(String[] args) {
         Stack1 st = new Stack1();
-        st.push(10);
-        st.push(20);
-        System.out.println(st.pop());
-        System.out.println(st.pop());
+        st.peek();
     }
 }
